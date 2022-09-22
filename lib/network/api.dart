90470e95b5f12fbb63638d3ab301b24a957ce21e
rahmat <rahmat@carakan.id>
2022-09-22 15:54:48 +0700

    up

--- lib/network/api.dart
+++ lib/network/api.dart
@@ -1,3 +1,2 @@
-const String BaseUrl = 'http://192.168.99.57:8080/api/v1';
+const String BaseUrl = 'http://192.168.99.62:8080/api/v1';
 const String loginAccount = '$BaseUrl/auth/login';
-const String registerAccount = '$BaseUrl/auth/register';
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,151 +1,48 @@
-import 'dart:convert';
-
 import 'package:auth_flutter/components/form_input.dart';
+import 'package:auth_flutter/components/logo_app.dart';
 import 'package:auth_flutter/constants.dart';
-import 'package:auth_flutter/network/api.dart';
-import 'package:auth_flutter/network/session_manager.dart';
-import 'package:auth_flutter/screens/home_page.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  GlobalKey<FormState> _keyForm = GlobalKey<FormState>();
-
-  bool isLoading = false;
-
-  Future _loginAccount() async {
-    setState(() {
-      isLoading = true;
-    });
-    try {
-      var user_password;
-      var user_email;
-      var res = await http.post(Uri.parse(loginAccount), body: {
-        'user_email': user_email.text,
-        'user_password': user_password.text
-      });
-      var data = jsonDecode(res.body);
-      if (res.statusCode == 200) {
-        setState(() {
-          isLoading = false;
-          sessionManager.savePref(data['user']['id'], data['user']['user_nama'], data['user']['user_email'], data['token']);
-          Navigator.pushAndRemoveUntil(context,
-              MaterialPageRoute(builder: (_) => Homepage()), (route) => false);
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text('Login Success'),
-            ),
-          );
-        });
-      } else if(res.statusCode == 400){
-        setState(() {
-          isLoading = false;
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('${data['data']['message']}')));
-        });
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text('${e.toString()}')));
-    }
-  }
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: SingleChildScrollView(
-          physics: ScrollPhysics(),
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [primaryColor, secondaryColor])),
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [primaryColor, secondaryColor])),
+          child: SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.all(defaultPadding),
-              child: Form(
-                key: _keyForm,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                      child: Image.asset('assets/images/logo_app.png'),
+              child: Column(
+                children: [
+                  LogoApp(),
+                  SizedBox(
+                    height: defaultPadding,
+                  ),
+                  Center(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                          color: bodyTextColor,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: 'Poppin'),
                     ),
-                    SizedBox(
-                      height: defaultPadding,
-                    ),
-                    Center(
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                            color: bodyTextColor,
-                            fontSize: 26,
-                            fontWeight: FontWeight.w700),
-                      ),
-                    ),
-                    SizedBox(
-                      height: defaultPadding,
-                    ),
-                    FormInput(),
-                    SizedBox(
-                      height: defaultPadding * 2,
-                    ),
-                    isLoading
-                        ? Center(
-                            child: CupertinoActivityIndicator(
-                              radius: defaultPadding,
-                              color: bodyTextColor,
-                            )
-                                // CircularProgressIndicator(color: bodyTextColor),
-                          )
-                        : MaterialButton(
-                            onPressed: () {
-                              if (_keyForm.currentState!.validate()) {
-                                _loginAccount();
-                              }
-                            },
-                            minWidth: MediaQuery.of(context).size.width,
-                            height: MediaQuery.of(context).size.height /
-                                defaultPadding,
-                            color: bodyTextColor,
-                            child: Text(
-                              'Login',
-                              style: TextStyle(
-                                  color: primaryColor,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w600),
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                    SizedBox(
-                      height: defaultPadding,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                      },
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            color: bodyTextColor,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    height: defaultPadding,
+                  ),
+                  FormInput(),
+                ],
               ),
             ),
           ),
